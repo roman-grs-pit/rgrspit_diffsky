af1ed40540852fd096ae316efb76f3ebe561bd8d
Andrew Hearin <ahearin@anl.gov>
2025-03-21 07:50:23 -0700

    Fix linter error

--- rgrspit_diffsky/data_loaders/tests/test_load_fake_abacus.py
+++ rgrspit_diffsky/data_loaders/tests/test_load_fake_abacus.py
@@ -1,7 +1,11 @@
 """ """
 
+import numpy as np
+
 from .. import load_fake_abacus as lfa
 
 
 def test_load_fake_abacus_halos():
     halos = lfa.load_fake_abacus_halos()
+    for x in halos:
+        assert np.all(np.isfinite(x))
