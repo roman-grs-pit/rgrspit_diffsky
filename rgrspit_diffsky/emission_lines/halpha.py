Correct emission line flux scaling from log substract to division

rgrspit_diffsky/emission_lines/halpha.py
@@ -23,4 +23,4 @@ def sfr_to_Halpha_KTC94(sfr):
     l_halpha : ndarray, shape (n, )
         Halpha luminosity in units of erg/s
     """
-    return sfr - np.log10(K98_HALPHA_COEF)
+    return sfr / K98_HALPHA_COEF

rgrspit_diffsky/emission_lines/oii.py
@@ -25,4 +25,4 @@ def sfr_to_OII3727_K98(sfr):
     l_oii : ndarray, shape (n, )
         OII luminosity at 3727 in units of erg/s
     """
-    return sfr - np.log10(K98_OII_COEF)
+    return sfr / K98_OII_COEF